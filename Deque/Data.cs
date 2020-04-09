76197963c26379b9af94b829cd53e15d077c8217
Marek Nagy <62247071@cuni.cz>
2020-04-09 17:26:06 +0200

    Changed implementation of Data Node to adding to end and removing from front

---
+++ Deque/Data.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal class Data<T>
+{
+    public static readonly int size = 64;
+    public int currentSize => end - start + 1;
+    // indicis of first and last ALREADY OCCUPIED places in array
+    internal int start, end;
+    // initialized with nulls inside
+    internal T[] arr = new T[size];
+
+    public Data(T item, bool inverted)
+    {
+        int index;
+        if (inverted)
+        {
+            index = size - 1;
+        }
+        else
+        {
+            index = 0;
+        }
+        start = index;
+        end = index;
+        arr[index] = item;
+    }
+
+    public T this[int index] {  get {
+            if (index < 0 || index > end - start + 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return arr[start + index];
+        } }
+
+    public void Add(T data, bool inverted)
+    {
+        if (inverted)
+        {
+            if (start == 0)
+            {
+                throw new InvalidOperationException("Cannot add more data here.");
+            }
+            else
+            {
+                arr[--start] = data;
+            }
+        }
+        else
+        {
+            if (end == size - 1)
+            {
+                throw new InvalidOperationException("Cannot add more data here.");
+            }
+            else
+            {
+                arr[++end] = data;
+            }
+        }
+    }
+
+    public void Remove(bool inverted)
+    {
+        if (inverted)
+        {
+            if ( end < 0 )
+            {
+                throw new InvalidOperationException("Cannot add more data here.");
+            }
+            else
+            {
+                // remove last item in Data Node
+                if (start == end)
+                {
+                    start = -1;
+                    end = -1;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+        }
+        else
+        {
+            if (start == size - 1)
+            {
+                throw new InvalidOperationException("Cannot add more data here.");
+            }
+            else
+            {
+                start++;
+            }
+        }
+    }
+    public T[] GetStoredValues()
+    {
+        T[] res = new T[currentSize];
+        Array.Copy(arr, start, res, 0, currentSize);
+        return res;
+    }
+}
--- Deque/Deque.cs
+++ Deque/Deque.cs
@@ -5,7 +5,9 @@ using System.Text;
 
 public class Deque<T> : IList<T>
 {
-    Node front, end;
+    bool inverted = false;
+    Dictionary<int, Data<T>> map = new Dictionary<int, Data<T>>();
+    int front, end;
     public uint size {get; private set;} = 0;
 
     public Deque()
@@ -14,13 +16,39 @@ public class Deque<T> : IList<T>
     }
     public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-    public int Count => throw new NotImplementedException();
+    public int Count { get {
+            int sum = 0;
+            foreach( var Value in map.Values)
+            {
+                sum += Value.currentSize;
+            }
+            return sum;
+    } }
 
-    public bool IsReadOnly => throw new NotImplementedException();
+    public bool IsReadOnly => false;
 
     public void Add(T item)
     {
-        throw new NotImplementedException();
+        if (size != 0)
+        {
+            var dataNode = map[end];
+            if (dataNode.end == Data<T>.size - 1)
+            {
+                map.Add(++end, new Data<T>(item, inverted));
+            }
+            else
+            {
+                dataNode.
+            }
+        }
+        else
+        {
+            var node = new Data<T>(item, inverted);
+            front = 0;
+            end = 0;
+            map.Add(0, node);
+            size++;
+        }
     }
 
     public void Clear()
