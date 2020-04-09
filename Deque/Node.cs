0c4b8a4f5cc15f65cadec7c9bf714ad65deb8bea
Marek Nagy <62247071@cuni.cz>
2020-04-09 11:58:06 +0200

    Add project files.

---
+++ Deque/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal class Node
+{
+    public const int size = 64;
+    internal byte start, end;
+    public Node previous { get; private set; } = null;
+    public Node next { get; private set; } = null;
+
+    internal Node()
+    {
+
+    }
+}
